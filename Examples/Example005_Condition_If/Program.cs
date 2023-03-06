1897585f6c3873c316519e973f4090e5fee6089b
unknown <tarantylhomie@gmail.com>
2023-03-06 21:56:24 +0300

    add Example 005 Condition If-Else

---
+++ Examples/Example005_Condition_If/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == " миша") // Без пробела в кавычках не срабатывает условие 
+{
+    Console.WriteLine("Ура, это же МИША!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
